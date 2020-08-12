621f8465b7f3cd1c32fd06807a2f97bffe39fa49
julien evano <julien.evano@audencia.com>
2020-08-12 15:29:04 +0200

    finish line

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :restaurant, presence: true
+  validates :content, presence: true
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5, 0] }, numericality: { only_integer: true }
+  validates :rating, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'belgian' }
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'belgian' }
+hello = { name: 'Hello', address: 'London', category: 'french' }
+bonjour =  { name: 'Bonjour', address: 'Paris', category: 'french' }
+goodbye =  { name: 'Goodbye', address: 'Marseille', category: 'french' }
+salut =  { name: 'Salut', address: 'Paris', category: 'japanese' }
+my_pizza = { name: 'My Pizza', address: 'New York', category: 'italian' }
+
+[dishoom, pizza_east, hello, bonjour, goodbye, salut, my_pizza].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "valide ? #{restaurant.valid?}"
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
